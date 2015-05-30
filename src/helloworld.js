0d753bbb6cf1baa65484eadec477635937ece0d6
David Kwak <dkwak@dkwak-mbp1.local>
2015-05-30 16:04:51 -0700

    first transition

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,20 +1,42 @@
 
 
 var Kumquat = React.createClass({
+  getInitialState: function() {
+    return {
+      state: 'intro'
+    };
+  },
+
+  render: function() {
+    if (this.state.state === 'intro') {
+      return <KumquatIntro onClick={this._handleIntroClick} />
+    }
+
+    return (
+      <div> Enter your name: </div>
+    );
+  },
+
+  _handleIntroClick: function() {
+    this.setState({state: 'main'});
+  },
+});
+
+var KumquatIntro = React.createClass({
   render: function() {
     return (
       <div>
         <div>
           Welcome to Kumquat
         </div>
-        <button>
+        <button
+          onClick={this.props.onClick}>
           Get started
         </button>
       </div>
     );
   },
-});
-
+})
 React.render(
   <Kumquat />,
   document.getElementById('root')
